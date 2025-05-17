1ac5729c7522864a96703a3adf0bc16439a8b36e
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-05-17 08:58:24 +0700

    update greeting message in AppService to include lorem ipsum text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h1>Hello World! Hoang Long Toi la ai?</h1>';
+    let s = 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+    return '<h1>Hello World! Hoang Long?</h1>\n' + s;
   }
 }

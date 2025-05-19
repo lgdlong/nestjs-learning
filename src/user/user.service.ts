0b7b4e43b77b552824699baf5623e49659ec4d1d
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-05-19 17:38:12 +0700

    go to 6.4

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  // Create new user
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 
-  findAll() {
-    return `This action returns all user`;
+  // Get all users
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().select('-password').exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  // Get one user by _id
+  async findOne(_id: string): Promise<User> {
+    const user = await this.userModel
+      .findOne({ _id })
+      .select('-password')
+      .exec();
+    if (!user) {
+      throw new NotFoundException(`User with _id ${_id} not found`);
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  // Update user by _id
+  async update(_id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const updatedUser = await this.userModel.findById(_id);
+    Object.assign(updatedUser, updateUserDto);
+    await updatedUser.save(); // ✅ sẽ tự tăng __v
+
+    if (!updatedUser) {
+      throw new NotFoundException(`User with _id ${_id} not found`);
+    }
+
+    return updatedUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  // Delete user by _id
+  async remove(_id: string): Promise<User> {
+    const deletedUser = await this.userModel.findOneAndDelete({ _id }).exec();
+
+    if (!deletedUser) {
+      throw new NotFoundException(`User with _id ${_id} not found`);
+    }
+
+    return deletedUser;
   }
 }
